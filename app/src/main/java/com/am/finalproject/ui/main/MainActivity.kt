c978e3f81b6179e179c66324388633c8e1c80711
ahmad <ahmad.mukhlas61@gmail.com>
2023-11-23 20:26:19 +0700

    initialization project

---
+++ app/src/main/java/com/am/finalproject/ui/main/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.am.finalproject.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.am.finalproject.R
+import com.am.finalproject.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+
+        binding.btnLogin.setOnClickListener {
+
+        }
+    }
+}
