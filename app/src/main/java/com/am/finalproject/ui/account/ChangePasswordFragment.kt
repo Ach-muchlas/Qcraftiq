1fab9b65cc50c3c724b4670c52662ebb35566da3
Ilyasa Adam N <ilyasagans@gmail.com>
2023-11-25 07:26:23 +0700

    Membuat UI Ubah Password

--- app/src/main/java/com/am/finalproject/ui/account/ChangePasswordFragment.kt
+++ app/src/main/java/com/am/finalproject/ui/account/ChangePasswordFragment.kt
@@ -1,4 +1,4 @@
-package com.am.finalproject.ui.akun
+package com.am.finalproject.ui.account
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -14,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [UbahPasswordFragment.newInstance] factory method to
+ * Use the [ChangePasswordFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class UbahPasswordFragment : Fragment() {
+class ChangePasswordFragment : Fragment() {
 	// TODO: Rename and change types of parameters
 	private var param1: String? = null
 	private var param2: String? = null
@@ -35,7 +35,7 @@ class UbahPasswordFragment : Fragment() {
 		savedInstanceState: Bundle?
 	): View? {
 		// Inflate the layout for this fragment
-		return inflater.inflate(R.layout.fragment_ubah_password, container, false)
+		return inflater.inflate(R.layout.fragment_change_password, container, false)
 	}
 
 	companion object {
@@ -45,12 +45,12 @@ class UbahPasswordFragment : Fragment() {
 		 *
 		 * @param param1 Parameter 1.
 		 * @param param2 Parameter 2.
-		 * @return A new instance of fragment UbahPasswordFragment.
+		 * @return A new instance of fragment ChangePasswordFragment.
 		 */
 		// TODO: Rename and change types and number of parameters
 		@JvmStatic
 		fun newInstance(param1: String, param2: String) =
-			UbahPasswordFragment().apply {
+			ChangePasswordFragment().apply {
 				arguments = Bundle().apply {
 					putString(ARG_PARAM1, param1)
 					putString(ARG_PARAM2, param2)
