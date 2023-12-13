a2c97adb6d4a0d40fcb81db40030e6cbdc98a255
Ari Mada <amadalarangga@gmail.com>
2023-12-13 13:57:37 +0700

    videoview to webview

--- app/src/main/java/com/am/finalproject/ui/details/DetailsActivity.kt
+++ app/src/main/java/com/am/finalproject/ui/details/DetailsActivity.kt
@@ -1,14 +1,14 @@
 package com.am.finalproject.ui.details
 
-
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.webkit.WebView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
+import com.am.finalproject.R
 import com.am.finalproject.databinding.ActivityDetailsBinding
 import com.am.finalproject.ui.adapter.ViewPagerAdapter
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
@@ -47,5 +47,10 @@ class DetailsActivity : AppCompatActivity() {
         TabLayoutMediator(binding.middletab, binding.viewPager2) { tab, position ->
             tab.text = titles[position]
         }.attach()
+
+        // Add this block to load a webpage in your WebView
+        val myWebView: WebView = findViewById(R.id.webView)
+        myWebView.loadUrl("https://www.example.com")
+        myWebView.settings.javaScriptEnabled = true
     }
 }
